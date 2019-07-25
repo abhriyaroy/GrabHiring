ef7009446964a207ada0b29a682d5823217f31f2
Abhriya <roy.abhriya@gmail.com>
2019-07-25 11:58:44 +0530

    Add database helper class

---
+++ app/src/main/java/com/example/grabhiring/data/database/DatabaseHelper.kt
@@ -0,0 +1,15 @@
+package com.example.grabhiring.data.database
+
+import com.example.grabhiring.data.model.NewsDataEntity
+import io.reactivex.Single
+
+interface DatabaseHelper {
+  fun getCachedNews(): Single<NewsDataEntity>
+}
+
+class DatabaseHelperImpl : DatabaseHelper {
+  override fun getCachedNews(): Single<NewsDataEntity> {
+
+  }
+
+}
