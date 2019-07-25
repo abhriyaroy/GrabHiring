5fb67b6d69518d71a8bbbb0b1a146db559cd8958
Abhriya <roy.abhriya@gmail.com>
2019-07-25 05:37:42 +0530

    Add dependencies

---
+++ app/src/main/java/com/example/grabhiring/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.grabhiring
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
