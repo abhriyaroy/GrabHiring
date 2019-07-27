ccb1e3f5b7d99292a845d0afc166b66f1941b603
Abhriya Roy <roy.abhriya@gmail.com>
2019-07-27 10:30:38 +0530

    Add enter and exit animation to detail screen

--- app/src/main/java/com/example/grabhiring/ui/details/DetailsActivity.kt
+++ app/src/main/java/com/example/grabhiring/ui/details/DetailsActivity.kt
@@ -1,13 +1,19 @@
 package com.example.grabhiring.ui.details
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.example.grabhiring.R
 
 class DetailsActivity : AppCompatActivity() {
 
   override fun onCreate(savedInstanceState: Bundle?) {
     super.onCreate(savedInstanceState)
+    overridePendingTransition(R.anim.slide_from_right, R.anim.no_change)
     setContentView(R.layout.activity_details)
+  }
+
+  override fun onBackPressed() {
+    overridePendingTransition(R.anim.no_change, R.anim.slide_to_right)
+    super.onBackPressed()
   }
 }
